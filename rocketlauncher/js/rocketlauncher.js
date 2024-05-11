c5a2953070cf936deb254c90eb87fa9c8f72e9b6
unistaja <8427397+unistaja@users.noreply.github.com>
2024-05-11 13:37:08 +0300

    add beep sound to rocket game when rocket touches moon

--- rocketlauncher/js/rocketlauncher.js
+++ rocketlauncher/js/rocketlauncher.js
@@ -6,10 +6,22 @@ const ctx = canvas.getContext("2d");
 
 let isFlying = false;
 const rocketSpeed = 0.2;
-
-
-
 let currentY = height - 350;
+let hasBeeped = false;
+let context = null;
+
+// beep code from https://stackoverflow.com/questions/879152/how-do-i-make-javascript-beep
+const beep = (freq = 200, duration = 300, vol = 100) => {
+  const oscillator = context.createOscillator();
+  const gain = context.createGain();
+  oscillator.connect(gain);
+  oscillator.frequency.value = freq;
+  oscillator.type = "sine";
+  gain.connect(context.destination);
+  gain.gain.value = vol * 0.01;
+  oscillator.start(context.currentTime);
+  oscillator.stop(context.currentTime + duration * 0.001);
+}
 
 // main animation loop
 let previousTimestamp = document.timeline.currentTime;
@@ -23,8 +35,13 @@ function animate(timestamp) {
 
   currentY = isFlying ? Math.max(currentY - elapsed * rocketSpeed, 275) : Math.min(currentY + elapsed * rocketSpeed, height - 350);
   if (currentY === 275) {
+    if (!hasBeeped) {
+      hasBeeped = true;
+      beep();
+    }
     drawSmiley();
   } else {
+    hasBeeped = false;
     drawMoon();
   }
   if (isFlying) {
@@ -38,5 +55,5 @@ function animate(timestamp) {
  }
  animate(previousTimestamp);
 
- window.addEventListener("keydown", ()=> {isFlying = true});
+ window.addEventListener("keydown", ()=> {isFlying = true; context = context ?? new AudioContext()});
  window.addEventListener("keyup", ()=> {isFlying = false});
